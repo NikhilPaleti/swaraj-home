9903c974c622ed9f4bfacbe815cfa99f5d2195de
Nikhil Paleti <49222020+NikhilPaleti@users.noreply.github.com>
2021-09-05 17:20:26 +0530

    Set-Up routing for the 4 major "Pages"
    Set-Up routing using React Router DOM in the app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-// import Test from './components/Test.js';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Team from  './components/Team';
+import BusinessPlan from './components/BusinessPlans';
+import Home from './components/Home';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className = "App">
-      
+      <Router>
+        <Switch>
+          <Route path="/" exact component= {Home}> </Route>
+          <Route path="/ourplan" component= {BusinessPlan}> </Route>
+          <Route path="/ourteam" component= {Team}> </Route>
+          <Route path="/gallery" component= {Gallery}> </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
